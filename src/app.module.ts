5548fbe66712c9708fab07cdd8ff3f5ee3554ad6
Xavier Canjura <fernanxavi58@gmail.com>
2025-11-11 22:25:16 -0600

    🔧chore: add connection with postgres database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppConfiguration } from './core/config/app.config';
+import { JoiValidationSchema } from './core/config/joi.validations';
+
+import { CoreModule } from './core/core.module';
+import { ClientModule } from './client/client.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // Environment Configuration
+    ConfigModule.forRoot({
+      load: [AppConfiguration],
+      validationSchema: JoiValidationSchema,
+    }),
+
+    // Database Configuration
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('db.host'),
+        port: configService.get<number>('db.port'),
+        database: configService.get<string>('db.name'),
+        username: configService.get<string>('db.user'),
+        password: configService.get<string>('db.password'),
+        autoLoadEntities: true,
+        synchronize: configService.get<string>('environment') === 'development',
+      }),
+    }),
+
+    // Module Imports
+    CoreModule,
+    ClientModule,
+  ],
 })
 export class AppModule {}
---
+++ src/core/config/joi.validations.ts
@@ -0,0 +1,10 @@
+import * as Joi from 'joi';
+
+export const JoiValidationSchema = Joi.object({
+  PORT: Joi.number().default(3000),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().default(5432),
+  DB_NAME: Joi.string().required(),
+  DB_USER: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+});
