9404e1414f5276a6543101f0be0e813732415742
Xavier Canjura <fernanxavi58@gmail.com>
2025-11-11 22:25:16 -0600

    🔧chore: add connection with postgres database

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+
+import { Logger } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common/pipes';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const logger = new Logger('Bootstrap');
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
+
+  logger.log('App is running on port ' + (process.env.PORT ?? 3000));
 }
+
 bootstrap();
